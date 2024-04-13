d694f0c1240df5069d1c8d63853b57c7e62691da
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-13 21:18:38 +0530

    feat accordion with single handle

--- src/app/react/layout.tsx
+++ src/app/react/layout.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-12">
-      <div className="lg:col-span-4 md:col-span-3">
+    <div className="grid grid-cols-12 min-h-screen w-full bg-slate-900">
+      <div className="lg:col-span-4 col-span-2 md:col-span-3">
         <ReactSideNav />
       </div>
-      <div className="col-span-12">{children}</div>
+      <div className="col-span-10 lg:col-span-8 md:col-span-9 ">{children}</div>
     </div>
   );
 }
--- src/app/react/page.tsx
+++ src/app/react/page.tsx
@@ -1,4 +1,15 @@
+import Accordion from "@/components/react/Accordion";
+
 function Page() {
-  return <div>Page</div>;
+  const componentArr = [<Accordion />];
+  return (
+    <main className="h-full w-full overflow-hidden overflow-y-scroll flex items-center justify-center bg-slate-700 text-white p-2">
+      {componentArr.map((component) => (
+        <div className="h-full w-full flex items-center justify-center">
+          {component}
+        </div>
+      ))}
+    </main>
+  );
 }
 export default Page;
