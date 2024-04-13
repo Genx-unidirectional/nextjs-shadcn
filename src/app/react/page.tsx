31e6fc94df7f16159661ac9c1a65a7d5611b4ce8
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-14 02:09:49 +0530

    feat star rating

--- src/app/react/page.tsx
+++ src/app/react/page.tsx
@@ -1,8 +1,9 @@
 import Accordion from "@/components/react/Accordion";
 import RandomColor from "@/components/react/RandomColor";
+import StarRating from "@/components/react/StarRating";
 
 function Page() {
-  const componentArr = [<Accordion />, <RandomColor />];
+  const componentArr = [<Accordion />, <RandomColor />, <StarRating />];
   return (
     <main className="h-full w-full overflow-hidden overflow-y-scroll bg-slate-800 text-white p-2">
       {componentArr.map((component, idx) => (
