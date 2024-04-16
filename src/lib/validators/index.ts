05f1eb3d9ecf2e053a7e9fc05576991a3eabd764
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-17 00:21:00 +0530

    feat zod validator for loadmore component

---
+++ src/lib/validators/index.ts
@@ -0,0 +1,21 @@
+import * as z from "zod";
+
+const ProductSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  description: z.string(),
+  price: z.number(),
+  discountPercentage: z.number(),
+  rating: z.number(),
+  stock: z.number(),
+  brand: z.string(),
+  category: z.string(),
+  thumbnail: z.string().url(),
+  images: z.array(z.string().url()),
+});
+
+const ProductsSchema = z.array(ProductSchema);
+
+export type ProductStateType = z.infer<typeof ProductSchema>
+
+export default ProductsSchema;
