8ff2ad17ed2831b616dc05381f68dd5b1401024d
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-19 19:07:53 +0530

    feat dummy api for features data

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,25 @@ export function delay(time: number) {
     }, time);
   });
 }
+export type FeaturesType = {
+  TicTacToe: boolean;
+  TabTest: boolean;
+  RandomColor: boolean;
+  QrCode: boolean;
+  Accordion: boolean;
+};
+
+const features: FeaturesType = {
+  TicTacToe: true,
+  TabTest: true,
+  RandomColor: true,
+  QrCode: true,
+  Accordion: true,
+};
+
+export function dummyFeaturesApi(): Promise<FeaturesType> {
+  return new Promise((resolve, reject) => {
+    if (features) setTimeout(() => resolve(features), 500);
+    else reject("some error occur while ");
+  });
+}
