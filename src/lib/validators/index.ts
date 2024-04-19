28e6aa8a502b096893e489480cda0a4e63acc7f1
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-19 10:27:20 +0530

    feat validator for user

--- src/lib/validators/index.ts
+++ src/lib/validators/index.ts
@@ -16,6 +16,43 @@ const ProductSchema = z.object({
 
 const ProductsSchema = z.array(ProductSchema);
 
-export type ProductStateType = z.infer<typeof ProductSchema>
+export type ProductStateType = z.infer<typeof ProductSchema>;
 
 export default ProductsSchema;
+
+const UserSchema = z.object({
+  login: z.string(),
+  id: z.number(),
+  node_id: z.string(),
+  avatar_url: z.string().url(),
+  gravatar_id: z.string(),
+  url: z.string().url(),
+  html_url: z.string().url(),
+  followers_url: z.string().url(),
+  following_url: z.string().url(),
+  gists_url: z.string().url(),
+  starred_url: z.string().url(),
+  subscriptions_url: z.string().url(),
+  organizations_url: z.string().url(),
+  repos_url: z.string().url(),
+  events_url: z.string().url(),
+  received_events_url: z.string().url(),
+  type: z.string(),
+  site_admin: z.boolean(),
+  name: z.string().nullable(),
+  company: z.string().nullable(),
+  blog: z.string().nullable(),
+  location: z.string().nullable(),
+  email: z.string().nullable(),
+  hireable: z.boolean(),
+  bio: z.string().nullable(),
+  twitter_username: z.string().nullable(),
+  public_repos: z.number(),
+  public_gists: z.number(),
+  followers: z.number(),
+  following: z.number(),
+  created_at: z.string(),
+  updated_at: z.string(),
+});
+
+export type UserType = z.infer<typeof UserSchema>;
